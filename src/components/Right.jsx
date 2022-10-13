556be0b73ffe3e76da48f3102344073bea6fcfc0
KayodeOusi <kayodeolusi@gmail.com>
2022-10-13 18:07:22 +0100

    fixed the error in Input component

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,50 +1,91 @@
-import React,{useState} from 'react'
-import Input from './Input'
+import React, { useState } from "react";
+import Input from "./Input";
 
 export default function Right() {
-  const [data,setData] = useState({
+  const [data, setData] = useState({
     firstname: "",
     lastname: "",
-    email:"",
-    password:"",
-    confirm:"",
-    phonenumber:""
+    email: "",
+    password: "",
+    confirm: "",
+    phonenumber: "",
   });
-  function update(event){
-    const {name,value} = event.target
-    setData(oldvalue=>{
+
+  function update(event) {
+    const { name, value } = event.target;
+    setData((oldvalue) => {
       return {
         ...oldvalue,
-        [name] : value
-      }
-    })
+        [name]: value,
+      };
+    });
   }
+
   return (
-    <article className='right'>
+    <article className="right">
       <div>
-        <p className='pfisrt'>This is not real online service!You know you need something 
-        like this in your life to help you realixe your deppest dream. sign up now to get started.</p>
+        <p className="pfisrt">
+          This is not real online service!You know you need something like this
+          in your life to help you realixe your deppest dream. sign up now to
+          get started.
+        </p>
         <p>you know you want to</p>
       </div>
       <div>
         <h2>Let's do this!</h2>
         <div className="input-form">
-        <div>
-          <Input name="firstname" handle={update} data={data} label="FIRST NAME" type="text"/>
-          <Input name="email" data={data} label= "EMAIL" type="email" />
-          <Input  name="password" handle={update} data={data} label="PASSWORD" type="password"/>
-          <p>*password does not match</p>
-        </div>
-        <div>
-        <Input name="lastname" data={data} label="LAST NAME" type="text"/>
-        <Input name="phonenumber" data={data} label="PHONE NUMBER" type="text"/>
-        <Input name="confirm" handle={update} data={data} label="CONFIRM PASSWORD" type="password"/>
-        </div>
+          <div>
+            <Input
+              name="firstname"
+              handle={update}
+              data={data}
+              label="FIRST NAME"
+              type="text"
+            />
+            <Input
+              name="email"
+              data={data}
+              label="EMAIL"
+              handle={update}
+              type="email"
+            />
+            <Input
+              name="password"
+              handle={update}
+              data={data}
+              label="PASSWORD"
+              type="password"
+            />
+            <p>*password does not match</p>
+          </div>
+          <div>
+            <Input
+              name="lastname"
+              data={data}
+              handle={update}
+              label="LAST NAME"
+              type="text"
+            />
+            <Input
+              name="phonenumber"
+              data={data}
+              handle={update}
+              label="PHONE NUMBER"
+              type="text"
+            />
+            <Input
+              name="confirm"
+              handle={update}
+              data={data}
+              label="CONFIRM PASSWORD"
+              type="password"
+            />
+          </div>
         </div>
       </div>
       <div>
-        <button>Create Account</button>
+        <button onClick={() => console.log(data)}>Create Account</button>
       </div>
     </article>
-  )
+  );
 }
