e1d438fad87b7c134681ebd9838939510bae869e
Jbsmall17 <93714910+Jbsmall17@users.noreply.github.com>
2022-10-14 01:18:40 +0100

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,14 @@ export default function Input({label,type,name, data, handle, styles}) {
   return (
     <>
       <label>{label}</label><br></br>
-      <input style={styles} onChange={handle} name={name} value={data.name} data={data} className={label.split(" ")[0]}type={type}></input><br></br>
+      <input style={styles} 
+        onChange={handle} 
+        name={name} 
+        value={data.name} 
+        data={data} 
+        className={label.split(" ")[0]}
+        type={type}>
+      </input><br></br>
     </>
   );
 }
